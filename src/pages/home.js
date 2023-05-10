9a576565840dd0aca25b3140b04071dd462da790
Subhom Kundu <subhomkundu@gmail.com>
2023-05-10 15:22:42 +0100

    Admin page different button

--- src/pages/home.js
+++ src/pages/home.js
@@ -10,19 +10,23 @@ export default function homePage() {
   return (
     <div className="flex-1 text-xs sm:text-sm flex flex-col justify-center items-center gap-2 sm:gap-4">
       <h1 className="text-4xl capitalize">Home page {`${user}`}</h1>
-
-      <button
-        onClick={handleClick}
-        className="mt-4 px-4 py-2 bg-blue-800 text-white rounded  text-lg"
-      >
-        Job filter
-      </button>
-      <button
-        onClick={handleClick}
-        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded capitalize text-lg"
-      >
-        Add User Information
-      </button>
+      {user == "admin" ? (
+        <button className="mt-4 px-4 py-2 bg-blue-800 text-white rounded  text-lg">
+          Check user information
+        </button>
+      ) : (
+        <>
+          <button className="mt-4 px-4 py-2 bg-blue-800 text-white rounded  text-lg">
+            Job filter
+          </button>
+          <button
+            onClick={handleClick}
+            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded capitalize text-lg"
+          >
+            Add User Information
+          </button>
+        </>
+      )}
     </div>
   );
 }
